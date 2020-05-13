09c758dc07e1a7b178861a2606c385568ddb2ea6
Tarek <tfzein@gmail.com>
2020-05-13 13:10:54 +0200

    Add project files.

---
+++ ControlTower/Enums/Airlines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlTower.Enums
+{
+    public enum Airlines
+    {
+        [Description("Scandinavian Airlines")]
+        SK,
+        [Description("British Airways")]
+        BA,
+        [Description("Finnair")]
+        AY,
+        [Description("Icelandair")]
+        FI,
+        [Description("Lufthansa")]
+        LH,
+        [Description("Air Europa")]
+        UX
+    }
+}
---
+++ ControlTower/Flight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlTower
+{
+    class Flight
+    {
+        string flightCode;
+        string flightStatus;
+        string flightImage;
+        public Flight()
+        {
+            flightCode = FlightCode;
+            flightStatus = FlightStatus;
+            flightImage = FlightImage;
+
+        }
+        public string FlightCode { get; set; }
+        public string FlightStatus { get; set; }
+        public string FlightImage { get; set; }
+    }
+}
